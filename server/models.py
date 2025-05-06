c0cad3a8827350046b8649e13dd970592d1039b4
rfquejada <rfquejada@up.edu.ph>
2025-05-06 21:57:56 +0800

    feat: added article model

--- server/models.py
+++ server/models.py
@@ -1,7 +1,23 @@
+import uuid
 from pydantic import BaseModel
 from typing import List, Optional
+from sqlalchemy import Column, DateTime, String, func, UUID
+from config import Base
 
-class Article(BaseModel):
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    article_id = Column(String, primary_key=True, index=True)
+    title = Column(String, index=True)
+    link = Column(String, unique=True, index=True)
+    description = Column(String, nullable=True)
+    source_url = Column(String, nullable=False)
+    pubDate = Column(DateTime(timezone=True), server_default=func.now())
+    category = Column(String, nullable=False)
+    image_url = Column(String, nullable=True)
+
+class ArticleOut(BaseModel):
     title: str
     link: str
     description: Optional[str] = None
@@ -12,5 +28,5 @@ class Article(BaseModel):
 
 
 class NewsResponse(BaseModel):
-    articles: List[Article]
+    articles: List[ArticleOut]
     total_articles: int = 0
