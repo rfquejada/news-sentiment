5b8f0a69b622bad64d7d700f67604ead9739bcff
rfquejada <rfquejada@up.edu.ph>
2025-05-08 14:47:02 +0800

    chore: fixed/clean main

--- server/main.py
+++ server/main.py
@@ -11,11 +11,10 @@ async def root():
     return {"message": "Hello World"}
 
 origins = [
-    "http://localhost:5173",
+    "http://localhost:3000",
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://ics-star-app.vercel.app"
 ]
 
 app.add_middleware(
