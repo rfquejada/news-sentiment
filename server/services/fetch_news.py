7f2c506c034aeae210c22d4a3c46112969d3525f
rfquejada <rfquejada@up.edu.ph>
2025-06-13 14:22:46 +0800

    chore: adjusted file driectory

--- server/services/fetch_news.py
+++ server/services/fetch_news.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from config import NEWS_API_URL, NEWS_API_KEY
 from models import Article
